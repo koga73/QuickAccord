886e6f489ad349bfde2131b221f2e7ae9c4fec6a
AJ Savino <aj.savino@moxieusa.com>
2016-12-18 20:31:46 -0500

    added url to js

--- js/QuickAccord.js
+++ js/QuickAccord.js
@@ -1,5 +1,6 @@
 /*
 * QuickAccord v1.0.0 Copyright (c) 2016 AJ Savino
+* https://github.com/koga73/QuickAccord
 * 
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
